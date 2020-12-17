bfa3d400cbdd8646283636da9b0d3c9b1cc9554c
carltgoss <ctgoss1840@gmail.com>
2020-12-17 10:51:10 -0500

    Add project files.

---
+++ Marketplace.Data/MarketItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Marketplace.Data
+{
+    public class MarketItem
+    {
+        [Key]
+        public int MarketId { get; set; }
+    }
+}
---
+++ Marketplace.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Marketplace.MVC.Startup))]
+namespace Marketplace.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
